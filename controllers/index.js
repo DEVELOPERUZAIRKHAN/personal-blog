47c58a4816e007c2a0380f89b56b53e969f31d2b
Uzair Saleem <uzairsaleemdev@gmail.com>
2023-08-09 09:34:06 +0500

    The create controller is completed

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,6 @@
 const Joi = require("joi")
 const filestack = require("filestack-js")
 const{FILE_STACK_API_KEY} = require("../config/index")
-const crypto = require("crypto");
 const Blog = require("../models/blog")
 const blogController={
 
@@ -11,49 +10,70 @@ const blogController={
     async getAll(req,res,next){
 
     },
-
+    
     async create(req,res,next){
       
-      // const myStr = crypto.randomBytes(13).toString("hex");
-        // const blogSchema = Joi.object({
-        //     title:Joi.string().min(5).max(50).required(),
-        //     description:Joi.string().required(),
-        //     content:Joi.string().required(),
-        //     author:Joi.string().required().min(5).max(30),
-        //     photo:Joi.string().required()
-        // })
-
-
-      // const{error} =  blogSchema.validate(req.body)
-      // if(error){
-        // return next(error);
-      // }
-      
-      const client = filestack.init(FILE_STACK_API_KEY)
-      const {title,content} = req.body;
       const uploadedFile = req.file;
-     let photoResponse ;
+        const blogSchema = Joi.object({
+            title:Joi.string().min(5).max(50).required(),
+            description:Joi.string().required(),
+            content:Joi.string().required(),
+            author:Joi.string().required().min(5).max(30),
+        })
+
+
+      const{error} =  blogSchema.validate(req.body)
+      if(error){
+        return next(error);
+      }
+      //validating the photo
+const{title,content,description,author} = req.body;
+      if(!uploadedFile){
+        
+        const error = {
+          message:"File not included in the request",
+          status:400
+        }
+      return  next(error)
+      }
+      if(uploadedFile.mimetype!==('image/jpeg'||'image/jpg'||'image/png')){
+        const error ={
+          message:"The uploaded file is not image",
+          status:400
+        }
+      return  next(error)
+      }
+      if(uploadedFile.size>10*1024*1024){
+        const error = {
+          message:"The uploaded image size exceeded the limit",
+          status:400
+        }
+      return   next(error)
+      }
+
+      const client = filestack.init(FILE_STACK_API_KEY)
+     let photoResponse;
     try{
     photoResponse =  await client.upload(uploadedFile.buffer)
-    console.log(photoResponse)
-    console.log("url" ,photoResponse.url)
     }
     catch(error){
       return next(error)
     }
 
+let response;
+    try{
+      const blogToSave = new Blog ({
+        title,author,content,photo:photoResponse.url,description
+      })
 
-    // try{
-    //   const blogToSave = new Blog ({
-    //     title,author,content,photo:photoResponse.url,description
-    //   })
-
-      // let response = await blogToSave.save()  
-      res.status(201).json({url:photoResponse.url,content,title})
-    // }
-    // catch(error){ 
-    //   return next(error)
-    // }
+       response = await blogToSave.save()   
+    }
+    catch(error){ 
+      return next(error)
+    }
+    res.status(201).json({blog:{
+      response
+    }})
 
     },
     async delete(req,res,next){
